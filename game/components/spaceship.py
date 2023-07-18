babf9167b9c13d67ba16c4d165c9507a8358495d
luisdonanfer <129348030+LuisDonanfer@users.noreply.github.com>
2023-07-18 17:47:19 -0400

    Tarea 2

--- game/components/spaceship.py
+++ game/components/spaceship.py
@@ -3,7 +3,7 @@ import pygame
 from pygame.sprite import Sprite
 
 
-class Spaceship(sprite):
+class Spaceship(Sprite):
     SPACESHIP_WIDHT = 40
     SPACESHIP_HEIGHT = 60
     SPACESHIP_POS_X = SCREEN_WIDTH / 2
@@ -14,15 +14,23 @@ class Spaceship(sprite):
         self.image = pygame.transform.scale(self.image,(self.SPACESHIP_WIDHT, self.SPACESHIP_HEIGHT))
         self.rect = self.image.get_rect(midbottom =(self.SPACESHIP_POS_X, self.SPACESHIP_POS_Y))
 
-    def draw(self, screen):
-        screen.blit(self.image, self.rect)
 
     def update(self, user_input):
-        if user_input[pygame.K_LEFT and self.rect.left > 0]:
+        if user_input[pygame.K_LEFT]:
             self.rect.x -=10
-        elif user_input[pygame.K_RIGHT] and self.rect.right < SCREEN_WIDTH :
+            if self.rect.x < -40:
+                self.rect.x = SCREEN_WIDTH
+
+        elif user_input[pygame.K_RIGHT]: 
             self.rect.x +=10
-        elif user_input[pygame.K_UP] and self.rect.top > 300:
-            self.rect.x -=10
-        elif user_input[pygame.K_DOWM] and self.rect.bottom < SCREEN_HEIGHT:
-            self.rect.x +=10
+            if self.rect.x > SCREEN_WIDTH:
+                self.rect.x = -40
+
+        elif user_input[pygame.K_UP] and self.rect.top > 30:
+            self.rect.y -=10
+            
+        elif user_input[pygame.K_DOWN] and self.rect.bottom < SCREEN_HEIGHT:
+            self.rect.y +=10
+        
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
